120615c7e48cbdf93186d6d1dd6457ed28858e7b
obijuan <obijuan.cube@gmail.com>
2021-10-04 22:06:55 +0200

    Video 3.P03: Image added

---
+++ Wiki/Nivel-03/reto-1-3.asm
@@ -0,0 +1,81 @@
+;; Reto 1.2: Sprite de la vagoneta  
+
+org #4000
+run #4000
+
+;-- Dibujar Vagoneta
+ld hl,#C410
+ld (hl), #88
+
+ld h,#CC
+ld (hl), #F8
+
+ld h,#D4
+ld (hl), #FE
+
+ld h, #DC
+ld (hl), #FF
+
+ld h, #E4
+ld (hl), #BB
+
+ld h, #EC
+ld (hl), #15
+
+ld h, #F4
+ld (hl), #4A
+
+ld h, #FC
+ld (hl), #04
+
+inc hl
+
+ld h,#C4
+ld (hl), #11
+
+ld h,#CC
+ld (hl), #F1
+
+ld h,#D4
+ld (hl), #F7
+
+ld h, #DC
+ld (hl), #FF
+
+ld h, #E4
+ld (hl), #DD
+
+ld h, #EC
+ld (hl), #8A
+
+ld h, #F4
+ld (hl), #25
+
+ld h, #FC
+ld (hl), #02
+
+;;-- Dibujar el rail
+
+ld a, #50  ;; Contador para repeticion
+
+ld l, #60  ;; Posicion inicial
+
+repetir:
+  ld hl, #c4
+  ld (hl), #5F
+
+  ld h,#CC
+  ld (hl), #40
+
+  ld h, #D4
+  ld (hl), #60
+
+  ld h, #DC
+  ld (hl), #50
+
+  inc hl
+  dec a
+  jr nz, repetir
+
+;--- Terminar
+jr $
