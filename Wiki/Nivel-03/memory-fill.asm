7829c86c56f1645767f537ae71ca069bfea19f60
obijuan <obijuan.cube@gmail.com>
2021-10-04 20:44:54 +0200

    Video 3.P03: Image added

--- Wiki/Nivel-03/memory-fill.asm
+++ Wiki/Nivel-03/memory-fill.asm
@@ -5,18 +5,23 @@ run #4000
 
 ld hl, #C000  ;; Primera direccion de la memoria de video
 
-ld a,#7F  ;; Contador: posiciones a rellenar
+ld b,#45
+repetir2:
 
+ld a,#F0  ;; Contador: posiciones a rellenar
 repetir:
 
-;--- Pintar 4 pixeles ojos
-ld (hl), #FF
+  ;--- Pintar 4 pixeles ojos
+  ld (hl), #FF
 
-inc hl ;-- Apuntar a la siguiente posicion
-dec a  ;-- Decrementar contador
+  inc hl ;-- Apuntar a la siguiente posicion
+  dec a  ;-- Decrementar contador
 
-jr nz, repetir
+  jr nz, repetir
 
+dec b
+jr nz, repetir2
+  
 ;; Terminar (Bucle infinito)
 jr $
 
