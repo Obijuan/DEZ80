5c4e0c4fe5340ef75367e163dbf7953e14124f6c
obijuan <obijuan.cube@gmail.com>
2021-10-09 22:04:04 +0200

    solid box

--- RA2017/practica/src/main.s
+++ RA2017/practica/src/main.s
@@ -1,12 +1,20 @@
 .area _DATA
 
+;;-- CPCtelera symbols
+.globl cpct_drawSolidBox_asm
+
 .area _CODE
 
 ;;===============================
 ;;  Main program entry
 ;;===============================
 _main::
-  ld a, #0xFF
-  ld (0xC000),a
+
+  ;; Draw a Box
+  ld    de, #0xC320    ;; Location
+  ld     a, #0x0F      ;; Cyan
+  ld    bc, #0x0802    ;; Height, Width: 8x8 pixels
+  call  cpct_drawSolidBox_asm
 
   jr .
+
