02e2e439981461f208281ffa4262f3c927bc18a3
obijuan <obijuan.cube@gmail.com>
2021-10-16 16:40:14 +0200

    sv1: animation added

--- RA2017/practica/src/main.s
+++ RA2017/practica/src/main.s
@@ -9,41 +9,6 @@
 .globl _level0
 
 ;;===============================
-;; drawGround
-;;
-;; INPUT:
-;;   A => Color Pattern
-;;
-;; DESTROYs: AF, BC, DE, HL
-;;===============================
-drawGround:
-
-  push AF ;;-- Save the color pattern
-
-  ;;-- Draw the half-left side of the ground
-  ld   de, #0xC000  ;;-- Video initial address
-  ld   bc, #0x5C00  ;;-- Position: 0,0x5C
-  call cpct_getScreenPtr_asm
-
-  ex de, hl      ;;-- DE=video address for the ground
-  pop AF         ;;-- Color pattern
-  push AF        ;;-- Save the color pattern
-  ld bc, #0x0240 ;;-- B: height (Pixels), C: width [1-64]
-  call cpct_drawSolidBox_asm
-
-  ;;-- Draw the half-right side of the ground
-  ld   de, #0xC000  ;;-- Video initial address
-  ld   bc, #0x5C40  ;;-- Position: 0x40,0x5C
-  call cpct_getScreenPtr_asm
-
-  ex de, hl      ;;-- DE=video address for the ground
-  pop AF         ;;-- Color pattern
-  ld bc, #0x0210 ;;-- B: height (Pixels), C: width [1-64]
-  call cpct_drawSolidBox_asm
-
-  ret
-
-;;===============================
 ;;  Main program entry
 ;;===============================
 _main::
@@ -70,33 +35,29 @@ _main::
   push hl
   ld hl,#0xC000
   push hl
-  ld bc,#0x0000
-  ld de,#0x2028  
-  ld a,#0x28
-  call cpct_etm_drawTileBox2x4_asm
-  
-  ;;-- Draw the Ground
-  ;;ld a, #0xC3 ;;-- Pink (mode 0)
-  ;call drawGround
+  ld bc,#0x0000   ;;-- Tile 0,0
+  ld de,#0x2028   ;;-- Height/Width in tiles (of the map)
+  ld a,#0x28      ;;-- Number of tiles in the row
+  call cpct_etm_drawTileBox2x4_asm  ;; Draw the map!!
 
   main_loop:
 
     call hero_erase     ;; Erase the hero
-    ;;call obstacle_erase ;; Erase the bullet
+    call obstacle_erase ;; Erase the bullet
 
     call hero_update     ;; Update the Hero
-    ;;call obstacle_update ;; Update the bullet
+    call obstacle_update ;; Update the bullet
 
     ;;-- Check colision
     call hero_getPtrHL
-    ;;call obstacle_checkCollision
-    ;;ld (0xC000),a        ;;-- Draw collision
-    ;;ld (0xC001),a 
-    ;;ld (0xC002),a
-    ;;ld (0xC003),a
+    call obstacle_checkCollision
+    ld (0xC000),a        ;;-- Draw collision
+    ld (0xC001),a 
+    ld (0xC002),a
+    ld (0xC003),a
 
     call hero_draw      ;;-- Draw the hero
-    ;;call obstacle_draw  ;;-- Draw the bullet
+    call obstacle_draw  ;;-- Draw the bullet
   
     ;;-- Wait for the raster to finish
     call cpct_waitVSYNC_asm
